732e58196a5749eb019c1793edee351816b68e9d
Toby267 <Tobyhothersall@gmail.com>
2025-06-17 14:41:25 +0100

    Removed the Move.cpp file, made the representation of a move as a union of different move types

--- engine/include/board/Move.hpp
+++ engine/include/board/Move.hpp
@@ -1,8 +1,56 @@
 #pragma once
 
-class Move {
-private:
+#include <board/Board.hpp>
 
-public:
-
+enum MoveType {
+    NORMAL,
+    PROMOTION,
+    EN_PASSANT,
+    CASTLE
 };
+
+struct NormalMove {
+    MoveType flag;
+    SquareIndex startPos;
+    SquareIndex endPos;
+    PieceType pieceType;
+};
+
+struct PromotionMove {
+    MoveType flag;
+    SquareIndex startPos;
+    SquareIndex endPos;
+    PieceType newPieceType;
+};
+
+struct EnPassantMove {
+    MoveType flag;
+    SquareIndex startPos;
+    SquareIndex endPos;
+    SquareIndex killSquare;
+};
+
+struct CastleMove {
+    MoveType flag;
+    SquareIndex primaryStartPos;
+    SquareIndex primaryEndPos;
+    SquareIndex secondaryStartPos;
+    SquareIndex secondaryEndPos;
+};
+
+union Move {
+    NormalMove normalMove;
+    PromotionMove promotionMove;
+    EnPassantMove enPassantMove;
+    CastleMove castleMove;
+};
+
+// struct Move {
+//     SquareIndex primaryStartPos;
+//     SquareIndex primaryEndPos;
+//     SquareIndex secondaryStartPos;
+//     SquareIndex secondaryEndPos;
+//     SquareIndex killSquare;
+//     PieceType oldPieceType;
+//     PieceType newPieceType;
+// };
--- engine/src/board/Move.cpp
+++
@@ -1,1 +0,0 @@
-#include "board/Move.hpp"
