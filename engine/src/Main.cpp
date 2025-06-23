2478043d1558a60aed8a84da36c967e4e9661c3c
Toby267 <Tobyhothersall@gmail.com>
2025-06-23 10:51:21 +0100

    Added a todo list

--- engine/src/Main.cpp
+++ engine/src/Main.cpp
@@ -1,5 +1,12 @@
 /**
- * To move a piece at index 5 up by 1 square, I reomve it from index 5, and add a piece at index 5+8 as per the compass rose on chessprogramming.org 
+ * To move a piece at index 5 up by 1 square, I reomve it from index 5, and add a piece at index 5+8 as per the compass rose on chessprogramming.org
+ * 
+ * TODO:
+ * Move generation (without checking for check and the like)
+ * Bitscan for converting move bitboards to move structs
+ * Arbiter to make move generation check for check and the like
+ * Validate moves
+ * Game endings
  */
 
 #include <iostream>
