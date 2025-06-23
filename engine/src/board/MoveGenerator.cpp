db7c6faf9b6d8540b7bb1ff541cdb4e536c8846f
Toby267 <Tobyhothersall@gmail.com>
2025-06-23 17:18:29 +0100

    Previous

--- engine/src/board/MoveGenerator.cpp
+++ engine/src/board/MoveGenerator.cpp
@@ -24,17 +24,13 @@ std::vector<Move> generateMoves(Board& board, bool whiteTurn) {
     const std::array<uint64_t, 14>& bitBoards = board.getBitBoards();
 
     //generate king moves
-        //by calculation
+    //only checks for legal postitions by valid move directions & borders, doesn't check for empty squares or friendly squares or checks
     uint64_t pawnAttacks = bitBoards[PieceType::WHITE_KING];
     pawnAttacks |= eastOne(pawnAttacks) | westOne(pawnAttacks);
     pawnAttacks |= northOne(pawnAttacks) | southOne(pawnAttacks);
 
-    printf("0x%016llx\n", bitBoards[PieceType::WHITE_KING]);
-    printf("0x%016llx\n", pawnAttacks);
-
     //generate knight moves
-        //multiple knight attacks parallel version
-
+    //only checks for legal postitions by valid move directions & borders, doesn't check for empty squares or friendly squares or checks
     uint64_t knights = bitBoards[PieceType::WHITE_KING];
     uint64_t l1 = (knights << 8 ) & (uint64_t)(0x0000FFFFFFFFFFFF);
     uint64_t l2 = (knights << 16) & (uint64_t)(0x00FFFFFFFFFFFFFF);
@@ -42,22 +38,10 @@ std::vector<Move> generateMoves(Board& board, bool whiteTurn) {
     uint64_t r2 = (knights >> 16) & (uint64_t)(0xFFFFFFFFFFFFFF00);
     uint64_t h1 = l1 | r1;
     uint64_t h2 = l2 | r2;
-    uint64_t knightAttacks = (h1<<2)&(0xFCFCFCFCFCFCFCFC) | (h1>>2)&(0x3F3F3F3F3F3F3F3F) | (h2<<1)&(0xFEFEFEFEFEFEFE) | (h2>>1)&(0x7F7F7F7F7F7F7F7F);
-    //uint64_t knightAttacks = (h1<<2) | (h1>>2) | (h2<<1)&(0xFEFEFEFEFEFEFE) | (h2>>1)&(0x7F7F7F7F7F7F7F7F);
-
-    // uint64_t west, east, attacks;
-    // east     = eastOne (knights);
-    // west     = westOne (knights);
-    // attacks  = (east|west) >> 2;
-    // attacks |= (east|west) << 2;
-    // east     = eastOne (east);
-    // west     = westOne (west);
-    // attacks |= (east|west) >>  1;
-    // attacks |= (east|west) <<  1;
-
-    board.setBitBoard(knightAttacks, PieceType::BLACK_KING);
-    board.setBitBoard(knightAttacks, PieceType::BLACK_PIECES);
-
+    uint64_t knightAttacks =    (h1<<2)&(0xFCFCFCFCFCFCFCFC) |
+                                (h1>>2)&(0x3F3F3F3F3F3F3F3F) |
+                                (h2<<1)&(0xFEFEFEFEFEFEFEFE) |
+                                (h2>>1)&(0x7F7F7F7F7F7F7F7F);
 
     //generate pawn moves
         //push per side
