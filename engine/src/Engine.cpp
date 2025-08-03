4b77355c922f76c1fe45b476109f26061af23090
Toby267 <tobyhothersall@gmail.com>
2025-08-03 08:54:36 +0100

    Passed all perft tests

--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -16,28 +16,16 @@
  * Defualt constructor, takes no arguments and sets up default values for the engine
  */
 Engine::Engine() {    
-    for (int i = 1; i <= 15; i++) {
-        // board->setDefaultBoard();                                                                //position 1                    - bug at depth >7 somewhere..., maybe...
-        // parseFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0");        //position 2                    - bug at depth 5, should be: 193690690, is: 193690689,  of by: -1
-        parseFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");                                   //position 3                    - bug at depth 5, should be: 674624,    is: 674622,     of by: -2
-        // parseFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");            //position 4                    - bug at depth 5, should be: 15833292,  is: 15833240,   of by: -52
-            // parseFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1 ");       //alternate to position 4       - bug at depth 5, should be: 15833292,  is: 15833240,   of by: -52
-        // parseFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ");                 //position 5                    - passed
-        // parseFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ");   //position 6                    - bug at depth >6 somewhere..., maybe...
-        std::cout << "depth: " << i << " nodes: ";
-        std::cout << perft(i) << '\n';
-    }
+    // board->setDefaultBoard();                                                                //position 1                    - passed perf(7), further depths unfeasable
+    // parseFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0");        //position 2                    - passed
+    // parseFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");                                   //position 3                    - passed
+    // parseFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");            //position 4                    - passed
+        // parseFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1 ");       //alternate to position 4       - passed
+    // parseFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ");                 //position 5                    - passed
+    // parseFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ");   //position 6                    - passed perf(6), further depths unfeasable
 
-    // parseFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    // perftDivide(2);
-
-    // parseFen("8/8/8/6k1/5Pp1/8/8/3K2R1 b - f3 0 2");
-    // std::vector<Move> moves = generateMoves(*board, whiteTurn);
-    // for (auto& i : moves) {
-    //     board->makeMove(i);
-    //     printASCIIBoard();
-    //     board->unMakeMove(i);
-    // }
+    for (int i = 1; i <= 15; i++)
+        std::cout << "depth: " << i << " nodes: " << perft(i) << '\n';
 }
 
 Engine::~Engine() {
